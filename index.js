9feeb291bd9b86ea04dcf11ef67ce52bd9693c43
Raza <rtshareef@gmail.com>
2019-10-08 17:31:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+
 // Write your code here!
+let remove = document.getElementById("main")
+remove.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = 'Raza is the champion'
