ff0c664cc49fa9c8d081f36f91e140de6bca0712
Mark Del Mar <mark@1000martians.com>
2019-10-08 16:42:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let removeID = document.querySelector("#main")
+removeID.remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = 'boobs is the champion'
